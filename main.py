53bcc43d30101fec081591705ac94ac4b2a66aa1
Gordon Burns <gordonburns@live.co.uk>
2023-08-22 13:04:01 +0100

    Added Credit_Card class and card validation method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("hotels.csv")
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -27,7 +28,6 @@ class Reservation:
         self.customer_name = cust_name
         self.hotel = hotel_object
 
-
     def generate(self):
         """Generates the content for the reservation"""
         content = f"""
@@ -42,14 +42,31 @@ class Reservation:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiry, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiry, "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_ID = int(input("Enter the ID of the hotel: "))
 
 hotel = Hotel(hotel_ID)
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation = Reservation(cust_name=name, hotel_object=hotel)
-    print(reservation.generate())
+    credit_card = CreditCard(number="1234")
+    if credit_card.validate(expiry="12/26", holder="JOHN SMITH", cvc="123"):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation = Reservation(cust_name=name, hotel_object=hotel)
+        print(reservation.generate())
+    else:
+        print("There is a problem validating your card")
+        print(df_cards)
 else:
     print("Hotel is Full")
